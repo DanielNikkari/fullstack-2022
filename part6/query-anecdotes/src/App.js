0a6827fa04586b9f79c33768ad20ce0ffc7b86dc
DanielNikkari <daniel.nikkari@gmail.com>
2023-03-16 11:55:25 +0200

    Part6: Finished 6.20-6.22

---
+++ part6/query-anecdotes/src/App.js
@@ -0,0 +1,61 @@
+import { useQuery, useMutation, useQueryClient } from 'react-query'
+import { getAnecdotes, voteAnecdote } from './requests'
+
+import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
+
+const App = () => {
+
+  const queryClient = useQueryClient()
+
+  const voteMutation = useMutation(voteAnecdote, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('anecdotes')
+    }
+  })
+
+  const handleVote = (anecdote) => {
+    voteMutation.mutate({ ...anecdote, votes: anecdote.votes + 1 })
+  }
+
+  const anecdotes = useQuery(
+    'anecdotes',
+    getAnecdotes,
+    {
+      retry: 1
+    }
+  )
+
+  console.log(anecdotes)
+
+  if (anecdotes.isLoading) {
+    return <div>Loading...</div>
+  }
+
+  if (!anecdotes.isSuccess) {
+    return <div>anecdote service not available due to problems in server</div>
+  }
+
+  return (
+    <div>
+      <h3>Anecdote app</h3>
+    
+      <Notification />
+      <AnecdoteForm queryClient={queryClient} />
+    
+      {anecdotes.data.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
---
+++ part6/query-anecdotes/src/components/AnecdoteForm.js
@@ -0,0 +1,30 @@
+import { useMutation } from 'react-query'
+import { createAnecdote } from '../requests'
+
+const AnecdoteForm = ({ queryClient }) => {
+
+  const newAnecdoteMutation = useMutation(createAnecdote, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('anecdotes')
+    }
+  })
+
+  const onCreate = (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    newAnecdoteMutation.mutate({ content, votes: 0 })
+}
+
+  return (
+    <div>
+      <h3>create new</h3>
+      <form onSubmit={onCreate}>
+        <input name='anecdote' />
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+}
+
+export default AnecdoteForm
